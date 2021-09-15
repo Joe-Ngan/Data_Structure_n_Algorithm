2e3bc3d18b38125a66e9c622650619f56c4af744
yanjinzhou <yan.jinz@northeastern.edu>
2021-09-14 21:19:23 -0700

    Check the type of the primitive data. Sep 14, 2021

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numberOfStudentsInClass = 62;
+        Integer test = 62;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        max = max+1;
+
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+
+        float myFloat = 5.23f;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+
+        double myDouble = 5.23d;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        System.out.println(maxShort);
+        System.out.println(minShort);
+
+        System.out.println(maxFloat);
+        System.out.println(minFloat);
+
+        System.out.println(maxDouble);
+        System.out.println(minDouble);
+
+
+        char myChar = '\u00A9';
+        System.out.println("my character would be like " + myChar );
+
+        System.out.println("Hello World");
+        System.out.println("We");
+    }
+}
